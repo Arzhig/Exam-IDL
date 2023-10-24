5f34d5a3fc1c34c9775a5e73be73e6f111b675c8
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2023-10-24 14:44:16 +0200

    PlaceTest
    Creation of a PlaceTest class in a test package.

---
+++ IDL_DEBUG_TP1_2/src/test/PlaceTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlaceTest {
+
+	@Test
+	void testPlace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveTokens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTokens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
