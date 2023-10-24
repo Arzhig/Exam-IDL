3c019bd6b998fc6acbe6360aa144df6d0900b905
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2023-10-24 15:40:36 +0200

    PlaceTest
    Added tests for the required methods for the Place class

--- IDL_DEBUG_TP1_2/src/test/PlaceTest.java
+++ IDL_DEBUG_TP1_2/src/test/PlaceTest.java
@@ -1,44 +1,72 @@
 package test;
-
+import bad4debug.Place;
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+/**
+ * @author Quentin Rabilloud
+ */
 
 class PlaceTest {
 
 	@Test
 	void testPlace() {
-		fail("Not yet implemented");
+		
+		Place p1 = new Place();
+		Assertions.assertEquals(0, p1.getTokens());
 	}
 
 	@Test
 	void testPlaceInt() {
-		fail("Not yet implemented");
+		int n = -2;
+		Place p2 = new Place(n);
+		Assertions.assertEquals(Math.abs(n), p2.getTokens());
 	}
 
 	@Test
 	void testPlaceStringInt() {
-		fail("Not yet implemented");
+		int n = 5;
+		Place p3 = new Place(null, n);
+		Assertions.assertEquals(n, p3.getTokens());
+		Assertions.assertEquals("PLACE", p3.getName());
+		Place p4 = new Place("TPlace", n);
+		Assertions.assertEquals("TPlace", p4.getName());
+	}
+	
+	@Test
+	void testGetTokens() {
+		int n = 3;
+		Place p5 = new Place(n);
+		Assertions.assertEquals(n, p5.getTokens());
 	}
 
 	@Test
 	void testRemoveTokens() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetTokens() {
-		fail("Not yet implemented");
+		int n = 7;
+		
+		int j = 5;
+		Place p6 = new Place(n);
+		p6.removeTokens(j);
+		Assertions.assertEquals((n-j), p6.getTokens());
+		
+		int k = -2;
+		Place p7 = new Place(n);
+		p7.removeTokens(k);
+		Assertions.assertEquals(0, p7.getTokens());
+		
+		int l = 14;
+		Place p8 = new Place(n);
+		p8.removeTokens(l);
+		Assertions.assertEquals(0, p8.getTokens());
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+		int n = 5;
+		Place p9 = new Place(n);
+		String expectedName = "Place: " + p9.getName() + " = " + n + "\n";
+		Assertions.assertEquals(p9.toString(), expectedName);
 	}
 
 }
